1ab13f14ca8581a4f4d04264148c9b8db0b05558
steamproof <93405617+steamproof@users.noreply.github.com>
2026-02-14 17:58:16 -0800

    Finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+print("My name is Max!")
+print('I like dogs!')
+
+# print('Hi!")
+# Error: quotation marks mismatched
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,11 @@
+x = 5 # integer: whole numbers without a decimal
+print(x)
+
+favorite_food = "pizza" # string: text enclosed in quotes
+print(favorite_food)
+
+print(x, favorite_food) # You can print multiple things on one line by using a comma
+print("My favorite food is", favorite_food)
+
+y = 3.25 # float: number with a decimal point
+print(y, "dogs", 4)
