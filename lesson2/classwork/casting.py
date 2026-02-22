f2f663dac6b1de13607bc99264184dc5d8da2be2
steamproof <93405617+steamproof@users.noreply.github.com>
2026-02-21 17:58:15 -0800

    Finished lesson 2

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,15 @@
+num_str = "42"
+num_int = int(num_str)  # Converting a string to an integer
+print(num_int + 1)
+
+num_str = "3.14"
+num_float = float(num_str)  # Converting a string to a float
+print(num_float)
+
+float_num = 9.99
+int_num = int(float_num)  # Converting a float to an integer
+print(int_num)
+
+num = 100
+num_str2 = str(num)  # Converting an integer to a string
+print(num_str2)
