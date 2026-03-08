85b3abf871cc6ab2599ba2b91dda8fa2d09bcc17
steamproof <93405617+steamproof@users.noreply.github.com>
2026-03-07 17:59:27 -0800

    Finished lesson 4

---
+++ lesson3/classwork/random_number.py
@@ -0,0 +1,8 @@
+import random  # You must import the random module to use random number functions
+
+num = random.randint(1, 10)  # Picks a random number between 1 and 10 (inclusive)
+print(num)
+
+for i in range(10):
+    num2 = random.randint(1, 100)
+    print(num2)
